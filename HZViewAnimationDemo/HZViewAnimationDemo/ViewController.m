83973da78b005af25a5cd03a3451916002df47f9
王浩祯 <471692847@qq.com>
2019-04-12 11:54:59 +0800

    add sample gif
    add a sample gif

--- HZViewAnimationDemo/HZViewAnimationDemo/ViewController.m
+++ HZViewAnimationDemo/HZViewAnimationDemo/ViewController.m
@@ -37,6 +37,7 @@
         button.backgroundColor = [UIColor blackColor];
         [button addTarget:self action:@selector(showView)
          forControlEvents:UIControlEventTouchUpInside];
+        button.layer.cornerRadius = 5;
         _showButton = button;
     }
     return _showButton;
@@ -48,6 +49,7 @@
         [button setTitle:@"hide" forState:UIControlStateNormal];
         button.backgroundColor = [UIColor blackColor];
         [button addTarget:self action:@selector(hideView) forControlEvents:UIControlEventTouchUpInside];
+        button.layer.cornerRadius = 5;
         _hideButton = button;
     }
     return _hideButton;
@@ -56,15 +58,21 @@
 - (void)showView{
     view = [[UIView alloc]initWithFrame:CGRectMake(VIEW_WID/2 - 150, 100, 300, 200)];
     view.backgroundColor = [UIColor yellowColor];
+    view.layer.cornerRadius = 10;
     
     UILabel* label = [[UILabel alloc]initWithFrame:CGRectMake(50, 50, 200, 30)];
     label.backgroundColor = [UIColor redColor];
     label.text = @"This is a label on view";
+    label.layer.cornerRadius = 5;
+    label.layer.masksToBounds = YES;
+    label.textAlignment = NSTextAlignmentCenter;
     [view addSubview:label];
     
     UIButton* button = [[UIButton alloc]initWithFrame:CGRectMake(30, 100, 240, 40)];
     button.backgroundColor = [UIColor blackColor];
     [button setTitle:@"this is a button on view" forState:UIControlStateNormal];
+    button.layer.cornerRadius = 5;
+    
     [view addSubview:button];
     [self.view addSubview:view];
     [view HZAnimationShowDuration:0.5  Completed:^{
